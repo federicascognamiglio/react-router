112bcc7c6b24090492151f35d6aa320553a0ad30
federicascognamiglio <scognamigliofede99@gmail.com>
2025-01-09 16:50:04 +0100

    feat: about page completed

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,7 +1,14 @@
 function AboutPage() {
     return (
         <main>
-            <h1>About Us Page</h1>
+            <main className="my-home-bg vh-100">
+                <div className="container">
+                    <div className="position-absolute top-50 start-50 translate-middle d-flex flex-column align-items-center w-25 pt-5">
+                        <h1 className="text-center my-title pb-3">ABOUT US</h1>
+                        <p className="text-center my-title">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis soluta expedita, quaerat neque quas, quia excepturi omnis at autem accusamus optio et maxime voluptatem, architecto mollitia doloremque nihil explicabo asperiores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolor blanditiis enim! Accusantium excepturi iusto, aspernatur in ex incidunt amet obcaecati voluptatibus aut modi quae.</p>
+                    </div>
+                </div>
+            </main>
         </main>
     )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,11 +5,11 @@ function HomePage() {
     return (
         <main className="my-home-bg vh-100">
             <div className="container">
-                <div className="position-absolute top-50 start-50 translate-middle d-flex flex-column align-items-center">
+                <div className="position-absolute top-50 start-50 translate-middle-x d-flex flex-column align-items-center">
                     <h1 className="text-center my-title pb-3">JUST COOK</h1>
                     <p className="text-center my-title pb-5">Your daily inspo for easy, tasty and healthy meals. <br />
                     Cooking has never been easier!</p>
-                    <NavLink className="btn my-btn" to="/posts">Start cooking</NavLink>
+                    <a className="btn my-btn" href="/posts">Start cooking</a>
                 </div>
             </div>
         </main>
